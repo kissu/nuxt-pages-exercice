cdec04e221f9fe0ccf8deb760668ffbb3407827d
Konstantin BIFERT <hello@kissu.io>
2018-04-15 17:16:43 +0200

    Add UX back button

--- pages/products/_id/index.vue
+++ pages/products/_id/index.vue
@@ -1,4 +1,15 @@
 <template lang="pug">
   div
-    p Hi, this is product \#{{ $route.params.id }} !
+    p Hi, this is product \#{{ $route.params.id }}
+    button(@click='backProducts') Go back to main products page
 </template>
+
+<script>
+export default {
+  methods: {
+    backProducts() {
+      this.$router.push('/products')
+    }
+  }
+}
+</script>
