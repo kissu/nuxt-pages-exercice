5c6af24ad0247ea8d36df2222dbed79ac4088d5a
Konstantin BIFERT <hello@kissu.io>
2018-04-15 19:01:35 +0200

    Add component to regular page

---
+++ components/yolo.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  div
+    p test passed
+</template>
+
+<style scoped>
+  * {
+    background-color: red;
+  }
+</style>
--- pages/products/_id/index.vue
+++ pages/products/_id/index.vue
@@ -1,15 +1,21 @@
 <template lang="pug">
   .container
     div
+      yolo
       p Hi, this is product \#{{ $route.params.id }}
       button(@click="$router.push('/products')") Go back to main products page
 </template>
 
 <script>
+import yolo from '~/components/yolo'
+
 export default {
   validate(data) {
     return /^\d+$/.test(data.params.id)
   },
-  layout: 'products'
+  layout: 'products',
+  components: {
+    yolo
+  }
 }
 </script>
