9f08f9f73a1dde32cb9bd5ee118a511380b9e120
Konstantin BIFERT <hello@kissu.io>
2018-04-15 17:22:25 +0200

    Add faster way to bind click event

--- pages/products/_id/index.vue
+++ pages/products/_id/index.vue
@@ -1,15 +1,5 @@
 <template lang="pug">
   div
     p Hi, this is product \#{{ $route.params.id }}
-    button(@click='backProducts') Go back to main products page
+    button(@click="$router.push('/products')") Go back to main products page
 </template>
-
-<script>
-export default {
-  methods: {
-    backProducts() {
-      this.$router.push('/products')
-    }
-  }
-}
-</script>
